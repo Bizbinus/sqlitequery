6eca454bb20bd46e69b3143e183309c9b228bdd8
Rui Sparks <Bizbin@Ruis-MacBook-Pro.local>
2017-12-17 08:54:10 -0800

    init

---
+++ SQLiteDataSwift/SQLiteDataSwift.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteDataSwift.h
+//  SQLiteDataSwift
+//
+//  Created by Rui Sparks on 12/17/17.
+//  Copyright © 2017 Bizbin LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLiteDataSwift.
+FOUNDATION_EXPORT double SQLiteDataSwiftVersionNumber;
+
+//! Project version string for SQLiteDataSwift.
+FOUNDATION_EXPORT const unsigned char SQLiteDataSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteDataSwift/PublicHeader.h>
+
+
